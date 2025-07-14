e7499245cec8dee960e4194dee992642e695aa51
Jan Tuzlic Offermann <offermannjan@yahoo.com>
2025-07-14 13:36:49 +0200

    adding some new functionality for handling PDG codes and fetching particle properties, using official pdg API

--- pdg.py
+++ pdg.py
@@ -1,3 +1,111 @@
+import ROOT as rt
+import numpy as np
+import pdg # the official PDG API
+
+class DatabasePDG(rt.TDatabasePDG):
+    """
+    This class is basically a combination of ROOT's TDatabasePDG,
+    and the official PDG API. Amazingly, I have found *both* to be
+    incomplete; for example TDatabasePDG lacks particles such as
+    f_0(500) (PDGID=9000221), but the official pdg API also has
+    issues for uu_1 (PDGID=2203). Combining them hopefully mitigates
+    this issue.
+    """
+
+    # This code is quite hacky! - Jan
+
+    def __init__(self):
+        super().__init__()
+        self.pdg_api = pdg.connect()
+
+    def GetParticle(self,pdg_id:int):
+
+        particle_root = None # particle returned by TDatabasePDG
+        particle_pdg = None # particle returned by official PDG API
+
+        try:
+            particle_root = super().GetParticle(pdg_id)
+        except:
+            pass
+
+        try:
+            particle_class = "" # "ParticleClass" like "meson" or "lepton", possibly leaving blank for now
+            try:
+                particle_class = particle_root.ParticleClass()
+            except:
+                pass
+
+            particle_pdg_raw = self.pdg_api.get_particle_by_mcid(pdg_id)
+            # convert this into a TParticlePDG -- we'll do an imperfect
+            # conversion since there are certain fields we won't use.
+
+            anti_particle = 0
+            if('bar' in particle_pdg_raw.name):
+                anti_particle = 1
+
+            try:
+                lifetime = particle_pdg_raw.lifetime
+                is_stable = False
+                if(lifetime is None):
+                    is_stable = True
+                elif(np.isinf(particle_pdg_raw.lifetime)):
+                    is_stable = True
+            except: # for particles like the f_0(500), we may trigger the except -- assume such particles are unstable. #NOTE: Therefore referencing this lifetime is not recommended!
+                lifetime = 0.
+                is_stable = False
+
+            mass = 0.
+            try:
+                if(particle_pdg_raw.mass is not None):
+                    mass = particle_pdg_raw.mass
+            except:
+                pass
+
+            width = 0.
+            try:
+                if(particle_pdg_raw.width is not None):
+                    width = particle_pdg_raw.width
+            except:
+                pass
+
+            particle_pdg = rt.TParticlePDG(
+                particle_pdg_raw.name,
+                particle_pdg_raw.name,
+                mass,
+                is_stable,
+                width,
+                particle_pdg_raw.charge / 3., # TParticlePDG gives charge in units of \e|/3
+                particle_class, # "ParticleClass" like "meson" or "lepton", leaving blank for now
+                pdg_id,
+                anti_particle,
+                0
+            )
+        except:
+            pass
+
+        if(particle_pdg is not None):
+            return particle_pdg
+        return particle_root
+
+    def GetCharge(self,pdg_id:int):
+        """
+        Simplified function for getting charge.
+        In practice, using this is much faster than
+        a full particle lookup using GetParticle()
+        followed by a call to Charge().
+        """
+        try:
+            particle = super().GetParticle(pdg_id)
+            return particle.Charge()
+        except:
+            particle = self.pdg_api.get_particle_by_mcid(pdg_id)
+            return particle.charge / 3. # division by 3 to match TDatabasePDG behaviour
+
+######################################################################
+# Some old code below, relatively clunky stuff for plotting information
+# on particles and labeling them nicely. This should ultimately be
+# updated or removed. -Jan
+######################################################################
 # Map PDG codes to particle names, with ROOT/Latex formatting.
 pdg_names = {
     0 : 'None',
